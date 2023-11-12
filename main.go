51c054f7b220f412669e66e30f17696147b52d25
setkyar <setkyar16@gmail.com>
2023-11-12 22:41:59 +0800

    chi router and middleware added

--- main.go
+++ main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+	router.Get("/hello", basicHandler)
 	server := &http.Server{
 		Addr: ":5000",
-		Handler: http.HandlerFunc(basicHandler),
+		Handler: router,
 	}
 
 	err := server.ListenAndServe()
